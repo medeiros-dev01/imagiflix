ef316cb48251f3f3e4d3bf79fb6475f97d5976eb
Jardelino Medeiros <jardelinomedeiros@outlook.com>
2021-06-02 15:50:34 -0300

    feat(carousel movie): creating and styling the film carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
-    
-    <div className='m-auto antialised font-sans bg-black text-white'>
+    <div className="m-auto antialised font-sans bg-black text-white">
       <Hero />
       <Navbar />
+      <Carousel />
+      <Carousel />
+      <Carousel />
     </div>
   );
-}
+};
 
 export default App;
