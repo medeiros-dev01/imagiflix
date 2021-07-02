efd07ce34da6560ba46015f47cb5368bf6f765e5
Jardelino Medeiros <jardelinomedeiros@outlook.com>
2021-07-02 12:58:21 -0300

    feat(footer): create footer section

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -11,17 +11,23 @@ import { Movie } from "./../data/mock";
 
 import "./Poster.css";
 
-const Poster = ({ cover, poster_path, title, name, vote_average }: Movie, index: number) => {
-  const {IMAGEURL} = CONST;
+const Poster = (
+  { cover, poster_path, title, name, vote_average }: Movie,
+  index: number
+) => {
+  const { IMAGEURL } = CONST;
 
   return (
     <article
       className="relative transition-all duration-500 ease-in-out transform hover:scale-110"
       key={index}
     >
-      <img src={poster_path ? `${IMAGEURL}/w200/${poster_path}` : cover} alt={title} />
+      <img
+        src={poster_path ? `${IMAGEURL}/w200/${poster_path}` : cover}
+        alt={title}
+      />
       <div className="poster cursor-pointer absolute inset-0 w-full h-full py-8 px-4 grid place-items-center text-center leading-6 bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0 ">
-        <h2 className="text-2xl">{title ? title : name }</h2>
+        <h2 className="text-2xl">{title ? title : name}</h2>
         <FontAwesomeIcon icon={faPlayCircle} size="5x" />
         <Score value={vote_average} />
       </div>
